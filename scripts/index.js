ea290d1ac5781843a15f6340822263b8ecf72152
Sarah Handal <sarah.handal@gmail.com>
2024-11-07 00:56:37 -0600

    finalizacion del proyecto sprint 8

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,125 @@
-let popupForm = document.querySelector('#popupForm');
-let editButton = document.querySelector('.profile__info-edit-btn');
-let closeButton = document.querySelector('.popup__close-btn');
-let saveButton = document.querySelector('.popup__submit');
-let nameInput = document.querySelector('#name-input');
-let jobInput = document.querySelector('#job-input');
-let profileName = document.querySelector('.profile__info-header');
-let profileJob = document.querySelector('.profile__info-title');
+let popupForm = document.querySelector("#popupForm");
+let editButton = document.querySelector(".profile__info-edit-btn");
+let nameInput = document.querySelector("#name-input");
+let jobInput = document.querySelector("#job-input");
+let profileName = document.querySelector(".profile__info-header");
+let profileJob = document.querySelector(".profile__info-title");
+
+let addCardPopup = document.querySelector("#addCardPopup");
+let addButton = document.querySelector(".profile__add-btn");
+let titleInput = document.querySelector("#place__title-input");
+let linkInput = document.querySelector("#place__link-input");
 
 function openPopup(popup) {
-    popup.classList.add('popup_show');
-    nameInput.value = profileName.textContent; 
-    jobInput.value = profileJob.textContent;
+  popup.classList.add("popup_show");
 }
 
 function closePopup(popup) {
-    popup.classList.remove('popup_show');
+  popup.classList.remove("popup_show");
 }
 
-editButton.addEventListener('click', function () {
-    openPopup(popupForm);
+editButton.addEventListener("click", function () {
+  openPopup(popupForm);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 });
 
-closeButton.addEventListener('click', function () {
-    closePopup(popupForm);
+
+addButton.addEventListener("click", function() {
+  openPopup(addCardPopup);
+  titleInput.value = "";
+  linkInput.value = "";
 });
 
-popupForm.addEventListener('submit', function(evt) {
-    evt.preventDefault();
-    let nameValue = nameInput.value;
-    let jobValue = jobInput.value; 
-    profileName.textContent = nameValue;
-    profileJob.textContent = jobValue;
-    closePopup(popupForm);  
+document.querySelectorAll(".popup__close-btn").forEach(button => {
+  button.addEventListener("click", function () {
+    const popup = button.closest(".popup");  
+    closePopup(popup);
+  });
+});
+
+popupForm.querySelector(".popup__form").addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup(popupForm);  
+});
+
+addCardPopup.querySelector(".popup__form").addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const titleValue = titleInput.value;
+  const imageValue = linkInput.value;
+  createCard(titleValue, imageValue);
+  closePopup(addCardPopup);  
+});
+
+function createCard(title, imageLink) {
+  const cardContainer = document.querySelector(".elements"); 
+  const cardElement = document.createElement("div");
+  cardElement.classList.add("element");
+
+  cardElement.innerHTML = `
+    <button class="element__trash" type="button">
+      <img class="trash-bin" src="../images/trash-bin.svg" alt="icono de basura">
+    </button>
+    <img src="${imageLink}" alt="${title}" class="element__image">
+    <h2 class="element__title">${title}</h2>
+    <div class="element__like"></div>
+  `;
+  cardContainer.prepend(cardElement); 
+}
+
+
+const trashButtons = document.querySelectorAll('.element__trash');
+const likeButtons = document.querySelectorAll('.element__like');
+
+trashButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    const element = e.target.closest('.element');
+    element.remove();
+  });
+});
+
+likeButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+   
+    const likeButton = e.currentTarget;
+    
+    if (likeButton.classList.contains('active')) {
+      likeButton.classList.remove('active');
+    } else {
+      likeButton.classList.add('active');
+    }
+  });
+});
+
+
+// 
+
+let popupImageCard = document.querySelector("#images-card");
+let popupImage = popupImageCard.querySelector(".popup__image-container");
+let popupImageTitle = popupImageCard.querySelector(".popup__image-title");
+
+
+function openImagePopup(imageSrc, imageAlt, imageTitle) {
+  popupImage.src = imageSrc;
+  popupImage.alt = imageAlt;
+  popupImageTitle.textContent = imageTitle;
+  openPopup(popupImageCard);
+}
+
+
+document.querySelectorAll(".element__image").forEach(image => {
+  image.addEventListener("click", function () {
+    const imageSrc = image.src;
+    const imageAlt = image.alt;
+    const imageTitle = image.nextElementSibling.textContent;  
+
+    openImagePopup(imageSrc, imageAlt, imageTitle);
+  });
+});
+
+
+popupImageCard.querySelector(".popup__close-btn").addEventListener("click", function () {
+  closePopup(popupImageCard);
 });
