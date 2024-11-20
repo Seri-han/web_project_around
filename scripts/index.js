7b77745ea641c5f7b7d084f3acfb174c2491aa43
Sarah Handal <sarah.handal@gmail.com>
2024-11-19 20:01:05 -0600

    add close functions with esc and click on the overlay

--- scripts/index.js
+++ scripts/index.js
@@ -127,3 +127,21 @@ document.querySelectorAll(".element__image").forEach(image => {
 popupImageCard.querySelector(".popup__close-btn").addEventListener("click", function () {
   closePopup(popupImageCard);
 });
+
+//
+
+function closePopupOnOverlay(evt) {
+  if (evt.target.classList.contains('popup_show')) {
+    closePopup(evt.target)
+  }
+}
+
+function closePopupOnEsc(evt) {
+  if (evt.key === 'Escape') {
+    const activePopup = document.querySelector('.popup_show');
+    if(activePopup) closePopup(activePopup);
+  }
+}
+
+document.addEventListener('mousedown', closePopupOnOverlay);
+document.addEventListener('keydown', closePopupOnEsc);
---
+++ scripts/validate.js
@@ -0,0 +1,29 @@
+const showInputError = (formElement, inputElement, errorMessage) => {
+
+}
+
+const hideInputError = (formElement, inputElement) => {
+
+}
+
+const checkInputValidity = (formElement, inputElement) => {
+
+}
+
+const hasInvalidInput = (inputList) => {
+
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+
+}
+
+const setEventListeners = ( formElement) => {
+
+}
+
+const enableValidation = () => {
+
+}
+
+enableValidation();
