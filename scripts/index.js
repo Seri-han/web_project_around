2707bd72b2ed58878c7bc8a05102beb97f3b81d3
Sarah Handal <sarah.handal@gmail.com>
2024-12-03 20:11:07 -0600

    Update index.js to edit cards and missing functions

--- scripts/index.js
+++ scripts/index.js
@@ -61,8 +61,41 @@ document.querySelectorAll(".popup__close-btn").forEach((button) => {
   });
 });
 
+
+
 document.addEventListener("mousedown", (evt) => {
   if (evt.target.classList.contains("popup_show")) {
     closePopup(evt.target);
   }
 });
+
+addCardPopup.querySelector(".popup__form").addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const titleValue = titleInput.value;
+  const imageValue = linkInput.value;
+  createCard(titleValue, imageValue);
+  closePopup(addCardPopup); 
+
+popupForm.querySelector(".popup__form").addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup(popupForm);
+});
+
+function createCard(title, imageLink) {
+  const cardContainer = document.querySelector('.elements');
+  const cardElement = document.querySelector('div');
+  cardElement.classList.add('element');
+
+  cardElement.innerHTML = `
+    <button class="element__trash" type="button">
+      <img class="trash-bin" src="../images/trash-bin.svg" alt="icono de basura">
+    </button>
+    <img src="${imageLink}" alt="${title}" class="element__image">
+    <h2 class="element__title">${title}</h2>
+    <div class="element__like"></div>
+  `;
+
+  cardContainer.prepend(cardElement);
+}
